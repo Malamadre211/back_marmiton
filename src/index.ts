7c897d48fe137176bcbc0cf6da5992fd9d095523
Malamadre211 <mariangelacurcioartist@gmail.com>
2023-09-15 15:59:10 +0200

    projetfunctionok

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express from "express"; 
+import express, { Request } from "express"; 
 import "dotenv/config";
 import cors from "cors";
 import bodyParser from "body-parser"
+import { Sequelize, DataTypes } from "sequelize";
 
 const app = express();
 
@@ -12,3 +13,48 @@ const port = parseInt(process.env.PORT as string);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
+
+  const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./db.sqlite",
+  })
+
+  const Recipe = sequelize.define("Recipe", {
+    name: {
+      type: DataTypes.STRING,
+    },
+    url: {
+        type: DataTypes.STRING,
+      },
+    duration: {
+      type: DataTypes.STRING,
+    },
+    note: {
+        type: DataTypes.STRING,
+    },
+  }, {
+    timestamps: false,
+  })
+
+  Recipe.sync();
+
+  interface IMaRequetBody {
+    name: string,
+    url: string,
+    duration: string,
+    note: string,
+  }
+  
+app.post("/recipes", async (req: Request<IMaRequetBody>, res) => {
+    const name = req.body.name
+    const url = req.body.url
+    const duration = req.body.duration
+    const note = req.body.note
+    const maRecette = await Recipe.create({name : name, duration : duration, note : note, url : url})
+    res.json(maRecette)
+  });
+
+app.get("/recipes", async (_, res) => {
+    const TousRecettes = await Recipe.findAll()
+    res.send(TousRecettes)
+})
